121c66d571de4dae238c2b9ed01375bba82c8be6
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-08-12 08:24:46 -0300

    feat: arrumado excluir ocm o prepared statements.

--- public/excluir.php
+++ public/excluir.php
@@ -1,7 +1,19 @@
 <?php
 include "../infra/conexao.php";
 $id = $_GET["id"];
-$sql = "DELETE FROM livros WHERE id=$id";
-mysqli_query($conexao,$sql);
+
+
+$stmt = mysqli_prepare(
+    $conexao
+
+
+"DELETE FROM livros WHERE id="?" ";
+
+);
+
+mysqli_stmt_bind_param($stmt, "i", $id);
+
+mysqli_stmt_execute($stmt);
+
 header("Location: ../index.php");
 ?>
